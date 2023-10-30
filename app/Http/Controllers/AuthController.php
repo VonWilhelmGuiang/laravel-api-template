608f02aac23a1189e71c208ac1998b2475af8c91
Von Wilhelm Guiang <guiang.vw@gmail.com>
2023-10-30 23:32:20 +0800

    Fixed Swagger

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,50 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
-/**
- * Required @OA\Info(title="deda", version="0.1")
- * @OA\SecurityScheme(
- *     type="http",
- *     securityScheme="bearerAuth",
- *     scheme="bearer",
- *     bearerFormat="JWT"
- * )
-*/
 class AuthController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     /**
-        *@OA\Post(
-        *   path="/api/login",
+        *Login 
+        *@OA\POST(
+        *   path="/api/auth/login",
         *   tags={"Auth"},
         *   summary="Authenticate user and generate JWT token",
         *   @OA\Parameter(
         *       name="email",
         *       in="query",
-        *       description="User's email",
+        *       description="User Email Address",
         *       required=true,
-        *       @OA\Schema(type="string")
         *   ),
-        *   @OA\Parameter(
-        *       name="password",
-        *       in="query",
-        *       description="User's password",
-        *       required=true,
-        *       @OA\Schema(type="string")
-        *   ),
-        *   @OA\Response(response="200", description="Login successful"),
+        *   @OA\Response(response="200", description="Login successful", @OA\JsonContent()),
         *   @OA\Response(response="401", description="Invalid credentials")
         *)
     */
-    public function login()
+    public function login(Request $request)
     {
-        
+        print_r($request->all());
+        return response()->json(['description' => 'Login successful'], 200);
     }
 }
